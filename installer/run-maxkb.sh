d0c6b71850f1afd328bd01dff84decc041e29457
lihelong <lihelong0309@163.com>
2025-05-20 16:14:07 +0800

    增加getToken

--- installer/run-maxkb.sh
+++ installer/run-maxkb.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 rm -f /opt/maxkb/app/tmp/*.pid
-# Start postgresql
-docker-entrypoint.sh postgres -c max_connections=${POSTGRES_MAX_CONNECTIONS} &
-sleep 10
-# Wait postgresql
-until pg_isready --host=127.0.0.1; do sleep 1 && echo "waiting for postgres"; done
-
+# Start PostgreSQL only if DISABLE_INTERNAL_PG is not true
+if [ "$DISABLE_INTERNAL_PG" != "true" ]; then
+    docker-entrypoint.sh postgres -c max_connections=${POSTGRES_MAX_CONNECTIONS} &
+    sleep 10
+    # Wait postgresql
+    until pg_isready --host=127.0.0.1; do sleep 1 && echo "waiting for postgres"; done
+else
+    echo "Skipping internal PostgreSQL startup."
+fi
 # Start MaxKB
 python /opt/maxkb/app/main.py start
